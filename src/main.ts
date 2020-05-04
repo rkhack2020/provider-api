2b928093c491b61cd3af2f97f85b134dbb1a3c2e
rkhack2020 <usa.raj89@gmail.com>
2020-05-04 20:31:44 +0530

    hostname

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const os = require("os");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+
+console.log(os.hostname());
   const url = await app.getUrl();
   console.log("Api URI - ",url);
 }
